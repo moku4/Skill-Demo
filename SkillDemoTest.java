5661be2b69fd517f902a77da6a1b3ec53e9362d5
youreyessuck <uareyesm@ucsd.edu>
2022-03-09 21:37:29 -0800

    update

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -1,7 +1,9 @@
-import java.util.*;
 import static org.junit.Assert.*;
 import org.junit.*;
 
 public class SkillDemoTest {
-
+    @Test
+    public void mult() {
+        assertEquals(2, SkillDemo.multiply(1, 3));
+    }
 }
