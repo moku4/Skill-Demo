91213298ae3b16857b137f5f03782f63a3e0715b
youreyessuck <uareyesm@ucsd.edu>
2022-03-09 21:43:42 -0800

    Debugging

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void mult() {
-        assertEquals(2, SkillDemo.multiply(1, 3));
+        assertEquals(2, SkillDemo.multiply(1, 2));
     }
 }
